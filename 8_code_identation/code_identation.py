15918197757656f0bf3e9e8da614e4cd3dd59392
Luis Filipe <learn2b3@gmail.com>
2025-10-16 20:37:47 +0000

    identatition

---
+++ 8_code_identation/code_identation.py
@@ -0,0 +1,17 @@
+
+#single 
+print("ABC")
+
+
+#block of code with identation
+if (2 < 5):
+	print ("Two is less than five")
+	print("Five is greater than two")
+	print("all right")
+
+print("Done the if!")
+ 
+
+# Use 2 or 4 spaces as recommended by the Python community
+# Set your editor's tab size to 2 or 4 spaces
+
