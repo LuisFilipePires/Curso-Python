ee0cd272d96ba761b28ea178babeedff60c2b879
Luis Filipe <learn2b3@gmail.com>
2025-10-16 18:47:43 +0000

    comparison operators

---
+++ 6_comparison_operators/comparison_operators.py
@@ -0,0 +1,30 @@
+
+x = 5
+y = 10
+
+print("x == y: ", x == y)
+print("x != y: ", x != y)
+print("x > y: ", x > y)
+print("x < y: ", x < y)
+
+z = 12
+
+print("5 <= z >= 10:", 5 <= z >= 10)
+
+#comparing strings
+
+word1 = "Hello"
+word2 = "World"
+
+print("word1 == word2: ", word1 == word2)
+print ("wor11[2] == word2[3]: ", word1[2] == word2[3])
+
+#float problem = false
+
+print("0.8 == (0.1 + 0.7)", 0.8 == (0.1 + 0.7))
+
+a = 5
+b = 2
+
+if (a > b) :		#True or False
+	print("a > b")
