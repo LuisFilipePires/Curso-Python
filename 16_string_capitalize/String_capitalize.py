6a369f0352952fa2577cb3867a65158d60a537a5
Luis Filipe <learn2b3@gmail.com>
2025-10-18 12:36:59 +0000

    String capitalize

---
+++ 16_string_capitalize/String_capitalize.py
@@ -0,0 +1,16 @@
+
+string = "portfolio COURSES"
+
+new_string = string.capitalize()
+
+print(new_string)
+
+#capitalize method creates a new string
+print(string)
+
+'''
+output: 
+
+Portfolio courses
+portfolio COURSES
+'''
