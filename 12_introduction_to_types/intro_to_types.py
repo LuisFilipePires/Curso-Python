0cab9a65081772f72c16f258fbab769dc5d68931
Luis Filipe <learn2b3@gmail.com>
2025-10-18 07:19:43 +0000

    data types

---
+++ 12_introduction_to_types/intro_to_types.py
@@ -0,0 +1,42 @@
+
+age = 23
+name = "Lay arnold"
+average = 29.5
+is_student = True
+grades = [92.3, 96.5, 89.9]
+
+print(type(age))
+print(type(name))
+print(type(average))
+print(type(is_student))
+print(type(grades))
+print(type(grades[1]))
+
+
+#       numeric
+#     /   |    \
+# Float  int  complex	
+
+nbr1 = "41"
+nbr2 = "50"
+sum = nbr1 + nbr2
+print(sum)
+#output: 4150
+
+sum = int(nbr1) + int (nbr2)
+print(sum)
+#output: 91
+
+'''
+output:
+
+<class 'int'>
+<class 'str'>
+<class 'float'>
+<class 'bool'>
+<class 'list'>
+<class 'float'>
+4150
+91
+
+'''
